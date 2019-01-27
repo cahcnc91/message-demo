64c69f539014bf65c488b32db98522a15fc1b7ef
Camila Coder <camilanierocosta@hotmail.com>
2019-01-26 20:32:17 -0500

    test deploy

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,17 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = [thunk];
 
+//const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore() {
   return createStore(
     rootReducer,
-    composeEnhancer(applyMiddleware(thunk))
+    composeWithDevTools(applyMiddleware(...middleware))
   );
 }
+
+
